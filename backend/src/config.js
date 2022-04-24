7f2164d25639df012c4ccd00ebb29a2db682d3bb
allan sumod-ong <allansumodong13@gmail.com>
2022-04-24 13:33:51 +0800

    222

--- backend/src/config.js
+++ backend/src/config.js
@@ -56,7 +56,7 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0xbc36ec4721C004a143362159326f04319454a846';
 const TREASURY_ADDRESS = '0xbc36ec4721C004a143362159326f04319454a846';
 const MAX_SUPPLY = 3333; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 3; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 0.01; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
